17e83aa9d3d22ecb27d46b700cda2e7134a69cf0
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2024-01-24 07:24:56 +0700

    Put get metods

--- src/Prises.java
+++ src/Prises.java
@@ -1,2 +1,19 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class Prises {
+
+    public PriorityQueue<Toy> queue;
+
+    public Prises() {
+        queue = new PriorityQueue<>(Comparator.comparingInt(Toy::getRareness));
+    }
+
+    public void Put(Toy toy){
+        queue.offer(toy);
+    }
+
+    public Toy Get(){
+        return queue.poll();
+    }
 }
--- src/Toy.java
+++ src/Toy.java
@@ -1,2 +1,31 @@
 public class Toy {
+    private int id;
+    private String name;
+    private int rareness;
+
+    public Toy(int id, String name, int luck) {
+        this.id = id;
+        this.name = name;
+        this.rareness = luck;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRareness() {
+        return rareness;
+    }
+    @Override
+    public String toString(){
+        return "Toy{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", rareness=" + rareness +
+                '}';
+    }
 }
